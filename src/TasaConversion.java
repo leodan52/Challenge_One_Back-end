e6087f598e46ddd49c1c0c46226a09601b4349ab
Leonardo D. Santiago <leodan.52@gmail.com>
2023-03-05 15:10:15 -0600

    Se modificaron las claves definidas para cada divisa para concordar con el resto del programa

--- src/TasaConversion.java
+++ src/TasaConversion.java
@@ -6,12 +6,12 @@ public class TasaConversion {
 	private Map<String, Double> conversor = new HashMap<>();
 	
 	public TasaConversion() {
-		this.conversor.put("mxn", 0.055666586);
-		this.conversor.put("usd", 1.0);
-		this.conversor.put("eur", 1.0631242);
-		this.conversor.put("gbp", 1.2041358);
-		this.conversor.put("jpy", 0.0073617702);
-		this.conversor.put("krw", 0.00077185536);
+		this.conversor.put("MXN", 0.055666586);
+		this.conversor.put("USD", 1.0);
+		this.conversor.put("EUR", 1.0631242);
+		this.conversor.put("GBP", 1.2041358);
+		this.conversor.put("JPY", 0.0073617702);
+		this.conversor.put("KRW", 0.00077185536);
 	}
 	
 	public double setTasa(String cantidadOrigen, String cantidadDestino) {
