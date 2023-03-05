02d1ac503adf4e40bc911eb2ce726714b5122735
Leonardo D. Santiago <leodan.52@gmail.com>
2023-03-05 15:11:05 -0600

    Testeado las nuevas modificaciones

--- src/TestConversor.java
+++ src/TestConversor.java
@@ -1,6 +1,7 @@
+import java.awt.EventQueue;
+
 import javax.swing.JOptionPane;
 
-import Ventanas.VentanaPrincipal;
 
 public class TestConversor {
 	public static void main(String[] args) {
@@ -12,22 +13,21 @@ public class TestConversor {
 //		
 //		System.out.println(conversor.ConvertirDivisas());
 		
-		VentanaPrincipal ventanaConversor;
 		String eleccion = mostrarMenuPrincipal();
-		String[] monedas = {"Peso mexicano (mxn)", "Dólar estadounidense (usd)", 
-				"Euro (eur)", "Libra estelina (gbp)", "Yen japonés (jpy)", "Won coreano (krw)"};
 		
 		System.out.println(eleccion);
 		
 		if (eleccion == "Conversor de monedas") {
-			ventanaConversor = new VentanaPrincipal();
-			
-			for (String moneda: monedas) {
-				ventanaConversor.divisaDestino.addItem(moneda);
-				ventanaConversor.divisaOrigen.addItem(moneda);
-			}
-			
-			ventanaConversor.frmConversorDeDivisas.setVisible(true);
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						VentanaDivisas window = new VentanaDivisas();
+						window.getFrmConversorDeDivisas().setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}); 
 		}
 		
 	}
@@ -38,4 +38,5 @@ public class TestConversor {
 		return (String) JOptionPane.showInputDialog(null, "Selecciones la acción que desea realizar",
 				"Menú principal", JOptionPane.DEFAULT_OPTION, null,  opcionesConversion, "");
 	}
+
 }
