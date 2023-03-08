da84581b5a3e13c0c9343424bfccf7ae086de877
Leonardo D. Santiago <leodan.52@gmail.com>
2023-03-07 21:29:43 -0600

    Este archivo será el archivo de ejecución principal

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.awt.EventQueue;
+
+import javax.swing.JOptionPane;
+
+import Ventanas.VentanaDivisas;
+
+public class Main {
+	public static void main(String[] args) {
+
+		String eleccion = mostrarMenuPrincipal();
+		
+		if (eleccion == "Conversor de monedas") {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						VentanaDivisas window = new VentanaDivisas();
+						window.getFrmConversorDeDivisas().setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}); 
+		}
+		
+	}
+	
+	private static String mostrarMenuPrincipal() {
+		String[] opcionesConversion = {"Conversor de monedas", "Conversor Magnitudes física"};
+		
+		return (String) JOptionPane.showInputDialog(null, "Selecciones la acción que desea realizar",
+				"Menú principal", JOptionPane.DEFAULT_OPTION, null,  opcionesConversion, "");
+	}
+
+}
