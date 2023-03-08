bf02c2e864a8bc5fb17c4e0153b363e97d1f2311
Leonardo D. Santiago <leodan.52@gmail.com>
2023-03-07 21:31:50 -0600

    Paquete especializado en clases que son necesarias para el calculo de conversiones de divisas

---
+++ src/divisas/ConversorMoneda.java
@@ -0,0 +1,68 @@
+package divisas;
+public class ConversorMoneda {
+
+	private double cantidad;
+	private String monedaOrigen;
+	private String monedaDestino;
+	private double tasa;
+	private double actualConversion = 0.0;
+	private String fechaActualización;
+
+	private TasaConversion tasas = new TasaConversion();
+
+	public ConversorMoneda() {
+
+	}
+
+	public ConversorMoneda(double cantidad, String monedaOrigen, String monedaDestino) {
+		this.cantidad = cantidad;
+		this.monedaOrigen = monedaOrigen;
+		this.monedaDestino = monedaDestino;
+	}
+
+	public double ConvertirDivisas() {
+		this.tasa = tasas.setTasa(this.monedaOrigen, this.monedaDestino);
+		this.actualConversion = this.cantidad * this.tasa;
+		this.fechaActualización = tasas.getFecha();
+		
+		return Math.round(this.actualConversion*1000.0)/1000.0;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getMonedaOrigen() {
+		return monedaOrigen;
+	}
+
+	public void setMonedaOrigen(String monedaOrigen) {
+		this.monedaOrigen = monedaOrigen;
+	}
+
+	public String getMonedaDestino() {
+		return monedaDestino;
+	}
+
+	public void setMonedaDestino(String monedaDestino) {
+		this.monedaDestino = monedaDestino;
+	}
+
+	public double getTasa() {
+		return Math.round(tasa*100000.0)/100000.0;
+	}
+
+	public double getActualConversion() {
+		return actualConversion;
+	}
+
+	public String getFechaActualización() {
+		return fechaActualización;
+	}
+	
+
+}
---
+++ src/divisas/TasaConversion.java
@@ -0,0 +1,86 @@
+package divisas;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class TasaConversion {
+
+	private Map<String, Double> conversor = new HashMap<>();
+	private int codigoStatus;
+	private String jsonRespuesta;
+	private boolean success;
+	private String fecha;
+	private JSONObject tasasJSON;
+
+	public TasaConversion() {
+		
+		try {
+			this.pedirTasasAPI();
+			this.procesarJSON();
+		} catch (IOException | URISyntaxException | InterruptedException e) {
+			this.success = false;
+			this.codigoStatus = 300;
+		}
+		
+		if (this.success && this.codigoStatus == 200 ) {
+			this.conversor.put("MXN", (double) this.tasasJSON.get("MXN"));
+			this.conversor.put("USD", 1.0);
+			this.conversor.put("EUR", (double) this.tasasJSON.get("EUR"));
+			this.conversor.put("GBP", (double) this.tasasJSON.get("GBP"));
+			this.conversor.put("JPY", (double) this.tasasJSON.get("JPY"));
+			this.conversor.put("KRW", (double) this.tasasJSON.get("KRW"));
+		} else {
+			this.conversor.put("MXN", 18.09536);
+			this.conversor.put("USD", 1.0);
+			this.conversor.put("EUR", 0.94826);
+			this.conversor.put("GBP", 0.84532);
+			this.conversor.put("JPY", 137.332017);
+			this.conversor.put("KRW", 1317.184996);
+		}
+	}
+
+	public double setTasa(String cantidadOrigen, String cantidadDestino) {
+		double tasaOrigen = this.conversor.get(cantidadOrigen);
+		double tasaDestino = this.conversor.get(cantidadDestino);
+
+		return  tasaDestino / tasaOrigen;
+
+	}
+
+	private void pedirTasasAPI() throws URISyntaxException, IOException, InterruptedException {
+		HttpClient client = HttpClient.newBuilder().build();
+
+		HttpRequest request = HttpRequest.newBuilder().uri(new URI(
+				"https://api.apilayer.com/exchangerates_data/latest?symbols=MXN%2C%20USD%2C%20EUR%2C%20GBP%2C%20JPY%2C%20KRW&base=USD"))
+				.header("apikey", "A8bt190xEnyx20L4FUGmQgAYpSvQjOCr").GET().build();
+
+		HttpResponse<String> respuesta = client.send(request, BodyHandlers.ofString());
+
+		this.jsonRespuesta = respuesta.body();
+		this.codigoStatus = respuesta.statusCode();
+	}
+	
+	private void procesarJSON() {
+		Object obt = JSONValue.parse(this.jsonRespuesta);
+		JSONObject json = (JSONObject) obt;
+		tasasJSON = (JSONObject) json.get("rates");
+		
+		success = (boolean) json.get("success");
+		fecha = (String) json.get("date");
+		
+	}
+	
+	public String getFecha() {
+		return fecha;
+	}
+	
+}
