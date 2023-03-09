4f208185234a1bf22f9c724e886e038764477e21
Leonardo D. Santiago <leodan.52@gmail.com>
2023-03-09 14:21:23 -0600

    Añadiendo control de excepciones. Solo se admiten números positivos

--- src/Ventanas/VentanaDivisas.java
+++ src/Ventanas/VentanaDivisas.java
@@ -1,4 +1,5 @@
 package Ventanas;
+
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
@@ -10,51 +11,52 @@ import java.util.regex.Pattern;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import divisas.ConversorMoneda;
 
 public class VentanaDivisas extends VentanaConversorDivisas {
-	
+
 	private ConversorMoneda conversor = new ConversorMoneda();
-	
+
 	public VentanaDivisas() {
 		super();
-		
+
 		this.agregarListaDivisas();
 		this.editarBotonInversion();
 		this.editarBotonConversor();
 		this.editarBotonCopiar();
 	}
-	
+
 	private void agregarListaDivisas() {
-		String[] monedas = {"", "Peso mexicano (MXN)", "Dólar estadounidense (USD)", 
-				"Euro (EUR)", "Libra esterlina (GBP)", "Yen japonés (JPY)", "Won coreano (KRW)"};
-		
-		for (String moneda: monedas) {
+		String[] monedas = { "", "Peso mexicano (MXN)", "Dólar estadounidense (USD)", "Euro (EUR)",
+				"Libra esterlina (GBP)", "Yen japonés (JPY)", "Won coreano (KRW)" };
+
+		for (String moneda : monedas) {
 			this.getEntradaDivisaOrigen().addItem(moneda);
 			this.getEntradaDivisaDestino().addItem(moneda);
 
 		}
 	}
-	
+
 	private void editarBotonInversion() {
-		
+
 		JComboBox<String> divisaOrigen = this.getEntradaDivisaOrigen();
 		JComboBox<String> divisaDestino = this.getEntradaDivisaDestino();
-		
+
 		this.getBotonInversor().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				Object seleccionadoOrigen = divisaOrigen.getSelectedItem();
 				Object seleccionadoDestino = divisaDestino.getSelectedItem();
-				
+
 				divisaOrigen.setSelectedItem(seleccionadoDestino);
 				divisaDestino.setSelectedItem(seleccionadoOrigen);
 			}
 		});
 	}
-	
+
 	private void editarBotonConversor() {
 		JTextField entrada = this.getCajaEntrada();
 		JComboBox<String> divisaOrigen = this.getEntradaDivisaOrigen();
@@ -62,59 +64,72 @@ public class VentanaDivisas extends VentanaConversorDivisas {
 		JLabel salida = this.getLabelSalida();
 		JLabel salidaUnidadCambio = this.getSalidaUnidadDivisa();
 		JButton copiar = this.getBotonCopiar();
-		
+
 		this.getBotonConversor().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				CharSequence seleccionadoOrigen = (CharSequence) divisaOrigen.getSelectedItem();
 				CharSequence seleccionadoDestino = (CharSequence) divisaDestino.getSelectedItem();
+				
 				String origen = "", destino = "";
 				double conversion, tasa;
-				
+
 				if (seleccionadoOrigen == "" || seleccionadoDestino == "" || entrada.getText() == "") {
 					return;
 				}
-				
+
 				Pattern pattern = Pattern.compile("(\\()(.*?)(\\))");
 				Matcher matcherOrigen = pattern.matcher(seleccionadoOrigen);
 				Matcher matcherDestino = pattern.matcher(seleccionadoDestino);
+				Double entradaCantidad;
 				
+				try {
+					entradaCantidad = Double.valueOf(entrada.getText());
+					if (entradaCantidad < 0) {
+						JOptionPane.showMessageDialog(copiar, "Por favor, ingrese solo valores positivos");
+						return;
+					}
+				} catch (NumberFormatException eString) {
+					JOptionPane.showMessageDialog(copiar, "Por favor, ingrese un valor numérico");
+					return;
+				}
+
 				while (matcherOrigen.find()) {
 					origen = matcherOrigen.group(2);
 				}
-				
+
 				while (matcherDestino.find()) {
 					destino = matcherDestino.group(2);
 				}
-				
-				conversor.setCantidad(Double.valueOf(entrada.getText()));
+
+				conversor.setCantidad(entradaCantidad);
 				conversor.setMonedaOrigen(origen);
 				conversor.setMonedaDestino(destino);
 				conversion = conversor.ConvertirDivisas();
 				tasa = conversor.getTasa();
-				
+
 				salida.setText(conversion + " " + destino);
 				salidaUnidadCambio.setText("1 " + seleccionadoOrigen + " = " + tasa + " " + seleccionadoDestino);
 				copiar.setEnabled(true);
-				
+
 			}
 		});
-		
+
 	}
-	
+
 	private void editarBotonCopiar() {
-		
+
 		this.getBotonCopiar().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				double cantidad = conversor.getActualConversion();
 				Clipboard click = Toolkit.getDefaultToolkit().getSystemClipboard();
 				StringSelection stringSelection = new StringSelection(String.valueOf(cantidad));
-				
+
 				click.setContents(stringSelection, null);
 			}
 		});
-		
+
 	}
-	
+
 }
