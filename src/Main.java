e929f09dc174848235651b4c86cf36d7947cf88b
Leonardo D. Santiago <leodan.52@gmail.com>
2023-03-10 17:55:30 -0600

    Se actualizó Main para agregar ventana para conversor de temperatura

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.awt.EventQueue;
 import javax.swing.JOptionPane;
 
 import Ventanas.VentanaDivisas;
+import Ventanas.VentanaTemperatura;
 
 public class Main {
 	public static void main(String[] args) {
 
 		String eleccion = mostrarMenuPrincipal();
-		
+
 		if (eleccion == "Conversor de monedas") {
 			EventQueue.invokeLater(new Runnable() {
 				public void run() {
@@ -19,16 +20,29 @@ public class Main {
 						e.printStackTrace();
 					}
 				}
-			}); 
+			});
+		} else if (eleccion == "Conversor de temperaturas") {
+			
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						VentanaTemperatura window = new VentanaTemperatura();
+						window.getFrmConversorDeTemperatura().setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			
 		}
-		
+
 	}
-	
+
 	private static String mostrarMenuPrincipal() {
-		String[] opcionesConversion = {"Conversor de monedas", "Conversor Magnitudes física"};
-		
-		return (String) JOptionPane.showInputDialog(null, "Selecciones la acción que desea realizar",
-				"Menú principal", JOptionPane.DEFAULT_OPTION, null,  opcionesConversion, "");
+		String[] opcionesConversion = { "Conversor de monedas", "Conversor de temperaturas" };
+
+		return (String) JOptionPane.showInputDialog(null, "Selecciones la acción que desea realizar", "Menú principal",
+				JOptionPane.DEFAULT_OPTION, null, opcionesConversion, "");
 	}
 
 }
